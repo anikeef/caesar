fee624c1913135847cdb8910e861e4d3fa2f5882
Gennadiy <genaydzhan70@gmail.com>
2019-02-12 23:29:15 +0600

    remove big comment from caesar.rb

--- caesar.rb
+++ caesar.rb
@@ -1,27 +1,4 @@
 class String
-  # def caesar(step)
-  #   self.codepoints
-  #     .map do |char|
-  #       case char
-  #       when 65..90
-  #         char += step
-  #         while char > 90
-  #           char -= 26
-  #         end
-  #         char.chr
-  #       when 97..122
-  #         char += step
-  #         while char > 122
-  #           char -= 26
-  #         end
-  #         char.chr
-  #       else
-  #         char.chr
-  #       end
-  #     end
-  #     .join
-  # end
-
   def caesar(step)
     self.codepoints.map { |code| shift_symbol(code, step).chr(Encoding::UTF_8) }.join
   end
